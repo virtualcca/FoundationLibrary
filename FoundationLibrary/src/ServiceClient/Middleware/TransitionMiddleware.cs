4e0727d906c6baa03029a61ea62d612fd2a650ec
LeoLaw <myluobin@live.com>
2016-06-29 18:03:41 +0800

    添加默认的会话中间件
    Related Work Items: #76

---
+++ FoundationLibrary/src/ServiceClient/Middleware/TransitionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace ServiceClients.Middleware
+{
+    /// <summary>
+    ///     默认的会话中间件
+    ///     <remarks>该中间件应该位于处理管道的第一位</remarks>
+    /// </summary>
+    public class TransitionMiddleware : ServiceClientMiddleware
+    {
+        /// <summary>
+        ///     初始化中间件
+        /// </summary>
+        /// <param name="next"></param>
+        public TransitionMiddleware(ServiceClientMiddleware next) : base(next)
+        {
+        }
+
+        /// <summary>
+        ///     执行中间件处理.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override void Invoke(ServiceClientContext context)
+        {
+            Next.Invoke(context);
+        }
+    }
+}
