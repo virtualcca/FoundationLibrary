b0a7d6da828e309f89683fc3fe7fd4dc007f17b3
LeoLaw <myluobin@live.com>
2016-06-29 09:30:26 +0800

    ServiceClient添加AssemblyInfo
    Related Work Items: #67

---
+++ FoundationLibrary/src/ServiceClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DapperWrapper")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3686202-151c-493e-9a2a-e3b90b2a3f92")]
