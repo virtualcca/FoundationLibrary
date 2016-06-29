580047188f8203a1fdd80a2f69295f54bc53acc0
LeoLaw <myluobin@live.com>
2016-06-29 17:29:46 +0800

    添加ServiceClient中间件定义
    Related Work Items: #73

---
+++ FoundationLibrary/src/ServiceClient/Middleware/ServiceClientMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace ServiceClients.Middleware
+{
+    /// <summary>
+    ///     <see cref="ServiceClient" />的处理中间件
+    /// </summary>
+    public abstract class ServiceClientMiddleware
+    {
+        /// <summary>
+        ///     初始化中间件
+        /// </summary>
+        /// <param name="next"></param>
+        protected ServiceClientMiddleware(ServiceClientMiddleware next)
+        {
+            Next = next;
+        }
+
+        /// <summary>
+        ///     当前处理管道中的下一个中间件.
+        /// </summary>
+        protected ServiceClientMiddleware Next { get; set; }
+
+        /// <summary>
+        ///     执行中间件处理.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public abstract void Invoke(ServiceClientContext context);
+    }
+}
