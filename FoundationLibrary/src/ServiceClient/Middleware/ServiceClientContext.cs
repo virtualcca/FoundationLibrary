5a6cc566171b686b7df9d7be803789fc35301953
LeoLaw <myluobin@live.com>
2016-06-29 17:23:14 +0800

    新增ServiceClientContext
    Related Work Items: #72

---
+++ FoundationLibrary/src/ServiceClient/Middleware/ServiceClientContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+
+namespace ServiceClients.Middleware
+{
+    /// <summary>
+    ///     <see cref="ServiceClient"/> 请求的上下文
+    /// </summary>
+    public abstract class ServiceClientContext
+    {
+        /// <summary>
+        ///     请求Url
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        ///     请求的Http方法
+        /// </summary>
+        public HttpVerb HttpVerb { get; set; }
+
+        /// <summary>
+        ///     请求内容
+        /// </summary>
+        public HttpContent RequestContent { get; set; }
+
+        /// <summary>
+        ///     返回的数据
+        /// </summary>
+        public HttpResponseMessage ResponseMessage { get; set; }
+    }
+}
