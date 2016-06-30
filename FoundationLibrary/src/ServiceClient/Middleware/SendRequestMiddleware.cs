a84cb8b9b1ed1aefeaacbd507100f1894ae201bc
LeoLaw <myluobin@live.com>
2016-06-30 10:00:03 +0800

    新增SendRequestMiddleware
    Related Work Items: #74

---
+++ FoundationLibrary/src/ServiceClient/Middleware/SendRequestMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ServiceClients.Middleware
+{
+    /// <summary>
+    ///     请求发送中间件
+    /// </summary>
+    /// <remarks>
+    ///     此中间件应该位于处理管道的最后一位
+    /// </remarks>
+    public class SendRequestMiddleware : ServiceClientMiddleware
+    {
+        /// <summary>
+        ///     初始化中间件
+        /// </summary>
+        /// <param name="next"></param>
+        public SendRequestMiddleware(ServiceClientMiddleware next) : base(next)
+        {
+        }
+
+        /// <summary>
+        ///     执行中间件处理.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task Invoke(ServiceClientContext context)
+        {
+            HttpResponseMessage result;
+            switch (context.HttpVerb)
+            {
+                case HttpVerb.Get:
+                    result =
+                        await
+                            context.InternalHttpClient.GetAsync(context.Url).ConfigureAwait(false);
+                    break;
+                case HttpVerb.Post:
+                    result =
+                        await
+                            context.InternalHttpClient.PostAsync(context.Url, context.RequestContent).ConfigureAwait(false);
+                    break;
+                case HttpVerb.Put:
+                    result =
+                        await context.InternalHttpClient.PutAsync(context.Url, context.RequestContent).ConfigureAwait(false);
+                    break;
+                case HttpVerb.Delete:
+                    result =
+                        await
+                            context.InternalHttpClient.DeleteAsync(context.Url).ConfigureAwait(false);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(context.HttpVerb.ToString(), context.HttpVerb, null);
+            }
+            context.ResponseMessage = result;
+        }
+    }
+}
