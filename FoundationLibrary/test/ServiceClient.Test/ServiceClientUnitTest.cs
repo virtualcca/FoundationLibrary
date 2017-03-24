af3a689cabb4f5785afd95ef570f5e6108d01b5d
LeoLaw <myluobin@live.com>
2017-03-24 23:47:38 +0800

    删除会实际发送数据的Get请求的单元测试用例

--- FoundationLibrary/test/ServiceClient.Test/ServiceClientUnitTest.cs
+++ FoundationLibrary/test/ServiceClient.Test/ServiceClientUnitTest.cs
@@ -13,23 +13,6 @@ namespace ServiceClientTest
     public class ServiceClientUnitTest
     {
         /// <summary>
-        ///     多个并发Get请求
-        /// </summary>
-        /// <returns></returns>
-        [Fact]
-        public async Task Get_Concurrence()
-        {
-            var serviceClient = new ServiceClient();
-            var taskA = serviceClient.RequestAsync("http://www.bing.com", HttpVerb.Get, new { A = "a" });
-            var taskB = serviceClient.RequestAsync("http://www.bing.com", HttpVerb.Get, new { A = "a" });
-            var taskC = serviceClient.RequestAsync("http://www.bing.com", HttpVerb.Get, new { B = "b" });
-            var taskD = serviceClient.RequestAsync("http://www.bing.com", HttpVerb.Get, new TestClassA { A = "a" });
-            var taskE = serviceClient.RequestAsync("http://www.bing.com", HttpVerb.Get, new TestClassA { A = "a" });
-
-            await Task.WhenAll(taskA, taskB, taskC, taskD, taskE);
-        }
-
-        /// <summary>
         ///     数据转QueryString的并发测试
         /// </summary>
         [Fact]
