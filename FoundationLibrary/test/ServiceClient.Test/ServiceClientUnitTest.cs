0c33d7fb30467674937f58c2b6da98b2141441fe
LeoLaw <myluobin@live.com>
2017-03-24 23:24:46 +0800

    增加ServiceClient反序列化测试的用例

--- FoundationLibrary/test/ServiceClient.Test/ServiceClientUnitTest.cs
+++ FoundationLibrary/test/ServiceClient.Test/ServiceClientUnitTest.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using ServiceClients;
 using Xunit;
 using ServiceClient = ServiceClients.ServiceClient;
@@ -41,6 +46,23 @@ namespace ServiceClientTest
 
         }
 
+        /// <summary>
+        ///     反序列化测试
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task Deserialized()
+        {
+            var serviceClient = new ServiceClient(new DeserializedTestHttpHandler());
+            var result = await serviceClient.RequestAsync<ComplateClass>("http://www.bing.com", HttpVerb.Post, null);
+            Assert.Equal(result.A, "a");
+            Assert.Equal(result.B, "b");
+            Assert.NotNull(result.TestClassA);
+            Assert.NotNull(result.ListInt);
+            Assert.Null(result.TestClassB);
+            Assert.Contains(1, result.ListInt);
+        }
+
 
         public class TestClassA
         {
@@ -50,6 +72,39 @@ namespace ServiceClientTest
         public class TestClassB
         {
             public string B { get; set; }
+        }
+
+        public class ComplateClass
+        {
+            public string A { get; set; }
+
+            public string B { get; set; }
+
+            public TestClassA TestClassA { get; set; }
+
+            public TestClassB TestClassB { get; set; }
+
+            public List<int> ListInt { get; set; }
+        }
+
+        /// <summary>
+        ///     用于测试反序列化的Handler
+        /// </summary>
+        public class DeserializedTestHttpHandler : DelegatingHandler
+        {
+            /// <inheritdoc />
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StringContent(JsonConvert.SerializeObject(new ComplateClass
+                {
+                    A = "a",
+                    B = "b",
+                    ListInt = new List<int> { 1, 2, 3, 4 },
+                    TestClassA = new TestClassA { A = "A" }
+                }));
+                return Task.FromResult(response);
+            }
         }
 
     }
